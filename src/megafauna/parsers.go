18f1479d1e73d3e45b935c28242276a234c0a46c
Matthew Stone <matthew.d.stone@gmail.com>
2012-06-24 14:18:54 -0700

    make Parse public

--- src/megafauna/parsers.go
+++ src/megafauna/parsers.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 )
 
-func parse(fileName string) (biomes []Biome, err error) {
+func Parse(fileName string) (biomes []Biome, err error) {
 	b, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return nil, err
