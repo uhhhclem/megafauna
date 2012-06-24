b2cff9018d9536a8f84559fa142d2b0c534ad236
Matthew Stone <matthew.d.stone@gmail.com>
2012-06-24 14:38:07 -0700

    Added test data and test for biome parser

---
+++ src/megafauna_test/parsers_test.go
@@ -0,0 +1,17 @@
+package megafauna_test
+
+import (
+	"megafauna"
+    "testing"
+)
+
+func TestParseBiome(t *testing.T) {
+	biomes, err := megafauna.Parse("testdata/test_biomes.json")
+	if err != nil {
+		t.Errorf("An error occured: %v", err)
+	}
+	if len(biomes) == 0 {
+		t.Errorf("Expected some biomes")
+	}
+}
+
