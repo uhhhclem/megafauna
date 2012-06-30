3ed3194a8ee461c03e1253057be92b19b4ec0cb1
Robert Rossney <rbr@well.com>
2012-06-30 16:20:22 -0700

    Move Habitat to its own file

---
+++ src/megafauna/habitat.go
@@ -0,0 +1,11 @@
+package megafauna
+
+// Habitat is a space on the board that contains a slot for the biome, the predator triangle,
+// and the rooter triangle.
+type Habitat struct {
+	Latitude         *Latitude
+	ClimaxNumber     int
+	IsOrogeny		 bool
+	AdjacentHabitats []*Habitat
+	Biome            *Biome
+}
--- src/megafauna/megafauna.go
+++ src/megafauna/megafauna.go
@@ -5,12 +5,3 @@ type Latitude struct {
 	Name    string
 	Habitat []*Habitat
 }
-
-// Habitat is a space on the board that contains a slot for the biome, the predator triangle,
-// and the rooter triangle.
-type Habitat struct {
-	Latitude         *Latitude
-	ClimaxNumber     int
-	AdjacentHabitats []*Habitat
-	Biome            *Biome
-}
