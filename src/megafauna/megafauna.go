6e298f69a555feae133839dc93933a83c422fd55
Robert Rossney <rbr@well.com>
2012-07-01 14:33:38 -0700

    Add Shuffle function.

--- src/megafauna/megafauna.go
+++ src/megafauna/megafauna.go
@@ -1,5 +1,7 @@
 package megafauna
 
+import "math/rand"
+
 const BiomeTypeKeys = "LWO"
 const DietaryDNAKeys = "BGHIP"
 const LatitudeKeys = "THJAO"
@@ -10,4 +12,16 @@ type Latitude struct {
 	Key     string
 	Name    string
 	Habitat []*Habitat
+}
+
+// Shuffle implements the Fisher-Yates-Knuth shuffling algorithm.  It takes a slice of
+// strings (typically keys to maps of tiles or cards) and randomizes their order.  Note
+// that the caller is responsible for seeding the random number generator.
+func Shuffle(keys []string) {
+	length := len(keys)
+	for i, _ := range keys {
+		j := i + rand.Intn(length - i)
+		keys[i], keys[j] = keys[j], keys[i]
+	}
+
 }
